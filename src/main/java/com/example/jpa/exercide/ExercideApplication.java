96404b363e8cbef29d65e5f80c9cb6801ecb3308
Aman Pal <aman.pal@tothenew.com>
2025-07-26 19:36:03 +0530

    first commit

---
+++ src/main/java/com/example/jpa/exercide/ExercideApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpa.exercide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercideApplication.class, args);
+	}
+
+}
