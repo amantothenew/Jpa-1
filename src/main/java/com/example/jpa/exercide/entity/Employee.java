c811b31a1c14bb4bcd5a9be688dd6503e64861ee
Aman Pal <aman.pal@tothenew.com>
2025-07-26 19:50:50 +0530

    Added: Q2

--- src/main/java/com/example/jpa/exercide/entity/Employee.java
+++ src/main/java/com/example/jpa/exercide/entity/Employee.java
@@ -1,5 +1,47 @@
 package com.example.jpa.exercide.entity;
 
+import jakarta.persistence.*;
+
+import javax.annotation.processing.Generated;
+
+@Entity
 public class Employee {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer age;
+    private String address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
---
+++ src/main/java/com/example/jpa/exercide/repository/Employee.java
@@ -0,0 +1,7 @@
+package com.example.jpa.exercide.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Employee extends JpaRepository<Employee, Integer> {
+
+}
